c8c0a1b8746944757b25e6122150299e9b1ca77d
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-08-02 16:33:51 +0900

    first commit

---
+++ src/main/java/com/feigncall/FeignCallApplication.java
@@ -0,0 +1,13 @@
+package com.feigncall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignCallApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/feigncall/client/feignCall.java
@@ -0,0 +1,5 @@
+package com.feigncall.client;
+
+public class feignCall {
+
+}
